278ef4304923ac5cdcc02e4b39614ca4dde28dc8
JaygoliM <golidjay000@warren.k12.in.us>
2022-02-01 12:21:01 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Rect rect1 = new Rect(2,4);
+
+        System.out.println(rect1);
+
+        rect1.setWidth(5);
+        rect1.setLength(10);
+        System.out.println(rect1);
+    }
+}
