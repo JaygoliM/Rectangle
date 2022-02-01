237ec8ef29880def6046af71c9b92e2ec47ff314
JaygoliM <golidjay000@warren.k12.in.us>
2022-02-01 12:33:28 -0500

    pizza

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,10 +6,13 @@ public class Main {
 
         Rect rect1 = new Rect(2,4);
 
-        System.out.println(rect1);
+        System.out.println(rect1.getLength());
+        System.out.println(rect1.getWidth());
+
 
         rect1.setWidth(5);
         rect1.setLength(10);
-        System.out.println(rect1);
+        System.out.println(rect1.getLength());
+        System.out.println(rect1.getWidth());
     }
 }
